b01cdb2f97423f9dd0c27f633508b1301c140976
sooneoo <thesooneoo@gmail.com>
2026-08-17 23:22:01 +0200

    update project files

--- install-dep.sh
+++ install-dep.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-pacman -S base-devel git openssh wayland wayland-protocols wlroots0.20  libinput xorg-xwayland pixman libxkbcommon ttf-nerd-fonts-symbols-common ttf-font-awesome ttf-fira-code powerline-fonts ttf-jetbrains-mono-nerd    foot rofi-wayland kanshi brightnessctl pipewire pipewire-pulse playerctl waylock swayidle tllist 
+pacman -S base-devel git openssh wayland wayland-protocols wlroots0.20  libinput xorg-xwayland pixman libxkbcommon ttf-nerd-fonts-symbols-common ttf-font-awesome ttf-fira-code powerline-fonts ttf-jetbrains-mono-nerd    foot rofi-wayland kanshi brightnessctl pipewire pipewire-pulse playerctl waylock swayidle swaybg tllist 
 
 pacman -S bluez bluez-utils blueman
 
---
+++ vimrc
@@ -0,0 +1,13 @@
+set nu
+set encoding=utf8
+set ts=4
+set shiftwidth=4
+set expandtab
+set linebreak
+set switchbuf+=newtab,usetab
+
+syntax on
+colorscheme unokai
+
+autocmd BufRead,BufNewFile *.evo set filetype=evo
+
